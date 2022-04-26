c878049495b2150c130dc86e747b7abe78e69c27
eeesnghyun <szhyun2002@gmail.com>
2022-04-26 23:44:50 +0900

    액티비티 예제 실습2

--- SampleLifeCycle/app/src/main/java/com/example/samplelifecycle/MainActivity.java
+++ SampleLifeCycle/app/src/main/java/com/example/samplelifecycle/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.samplelifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,17 +57,43 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+
         println("onPause 호출");
+        saveState();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
         println("onResume 호출");
+        restoreState();
     }
 
     public void println(String data) {
         Toast.makeText(this, data, Toast.LENGTH_LONG).show();
         Log.d("Main", data);
+    }
+
+    protected void restoreState() {
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        if ((pref != null) && (pref.contains("name"))) {
+            String name = pref.getString("name", "");
+            nameInput.setText(name);
+        }
+    }
+
+    protected void saveState() {
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("name", nameInput.getText().toString());
+        editor.commit();
+    }
+
+    protected void clearState() {
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.commit();
     }
 }
